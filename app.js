1de4ad12bb023dd62e66a5d8e245ea8edd75de99
felipecmleite <felipemarsom@prof.educacao.sp.gov.br>
2024-09-29 18:30:18 -0300

    Versão final

---
+++ app.js
@@ -0,0 +1,26 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartão'
+    cartao.innerHTML = `
+    <div class="cartão__conteúdo">
+    <h3>${categoria}</h3>
+    <div class="cartão__conteúdo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartão__conteúdo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+    container.appendChild(cartao)
+    
+}
---
+++ perguntas.js
@@ -0,0 +1,127 @@
+criaCartao(
+    'Geometria' ,
+    'Para qualquer triângulo retângulo define-se que o resultado da divisão do cateto adjacente pela hipotenusa se chama?',
+    'Coseno'
+)
+
+criaCartao(
+    'Eletromagnetismo',
+    'Cargas em movimento geram campos magnéticos que podem interagir com outros campos magnéticos externos. Verdadeiro ou falso?',
+    'Verdadeiro'
+)
+
+criaCartao(
+    'Eletromagnetismo',
+    'O sentido da força magnética é encontrado pela regra da mão esquerda (Flemming) ou pela da mão direita. Verdadeiro ou falso?',
+    'Verdadeiro'
+)
+
+criaCartao(
+    'Geometria',
+    'O que é o diâmetro?',
+    'Diâmetro é o comprimento da reta que passa pelo centro e toca dois pontos na borda do círculo. A metade desse comprimento é chamada de raio.'
+)
+
+criaCartao(
+    'Geometria',
+    'Qual é o resultado aproximado obtido quando dividimos o comprimento/perímetro de uma circunferência pelo seu diâmetro?',
+    '3,14...'
+)
+
+criaCartao(
+    'Geometria',
+    'Para qualquer triângulo retângulo define-se que o resultado da divisão do cateto oposto pela hipotenusa se chama?',
+    'Seno'
+)
+
+criaCartao(
+    'Eletromagnetismo',
+    'O que é um eletroímã?',
+    'Um eletroímã é um dispositivo formado por um núcleo de ferro envolto por um solenoide (bobina). Quando uma corrente elétrica passa pelas espiras da bobina, cria-se um campo magnético, o qual faz com que os imãs elementares do núcleo de ferro se orientem, ficando assim imantado e, consequentemente, com a propriedade de atrair outros materiais ferromagnéticos.'
+)
+
+criaCartao(
+    'Eletromagnetismo',
+    'Defina solenoide.',
+    'Solenoides (bobina longa), quando percorridos por corrente elétrica, são fontes de campo magnético. Eles são formados por enrolamentos de fios condutores, espaçados uniformemente, concêntricos e no formato de um cilindro de raio constante.'
+)
+
+criaCartao(
+    'Movimento Circular',
+    'O que é a força centrípeta?',
+    'É a força responsável por manter um corpo em trajetória curvilínea. Ela segue a direção do raio (radial) e tem sentido apontando para o centro. Ela é responsável por alterar a direção do vetor velocidade tangencial a cada instante.'
+)
+
+criaCartao(
+    'Movimento Circular',
+    'Para órbitas elípticas como a de planetas, a força centrípeta é resultante da força de atração gravitacional entre os dois corpos. Verdadeiro ou falso?',
+    'Verdadeiro'
+)
+
+criaCartao(
+    'Eletromagnetismo',
+    'Que tipo de trajetória descreverá uma carga que se move perpendicularmente a um campo magnético?',
+    'Circular'
+)
+
+criaCartao(
+    'Eletromagnetismo',
+    'Cargas em movimento interagindo com campos magnéticos podem descrever trajetória helicoidal. Verdadeiro ou falso?',
+    'Verdadeiro'
+)
+
+criaCartao(
+    'Mecânica',
+    'Newton postulou que caso haja uma força resultante agindo sobre um corpo de certa massa, ele acelerará ou desacelerará, ou seja haverá variação de sua velocidade. Verdadeiro ou falso?',
+    'Verdadeiro'
+)
+
+criaCartao(
+    'Movimento Circular',
+    'Para se calcular a força centrípeta deve-se multiplicar a massa do corpo em trajetória circular e multiplicá-la pela:',
+    'Aceleração centrípeta'
+)
+
+criaCartao(
+    'Elétrica',
+    'Quais são os dois tipos de corrente existentes?',
+    'CA (corrente alternada) e CC (corrente contínua)'
+)
+
+criaCartao(
+    'Elétrica',
+    'Como a corrente contínua (CC) é gerada?',
+    'Ela é gerada em circuitos abastecidos por pilhas ou baterias e ou em painéis fotovoltaicos. Ela é caracterizada pelo fluxo contínuo de elétrons.'
+)
+
+criaCartao(
+    'Elétrica',
+    'Como a corrente alternada (CA) é gerada?',
+    'A corrente alternada é produzida por geradores de CA que são encontrados em quase todos os tipos de usinas de eletricidade. Esses geradores promovem a rotação de uma espira condutora em meio a um campo magnético na qual passa a agir uma força eletromotriz induzida.'
+)
+
+criaCartao(
+    'Mecânica',
+    'O que é velocidade?',
+    'A velocidade de um corpo é a distância que ele percorre por unidade de tempo.'
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
